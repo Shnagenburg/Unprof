21f836548c5b95044dfe7e3155030d59ea966e1c
Shnagenburg <shnagenburg@gmail.com>
2011-12-30 01:42:26 -0500

    - second commit

---
+++ Unprof/Unprof/VisualEffectManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Unprof
+{
+    class VisualEffectManager
+    {
+        List<Explosion> mExplosions;
+        public List<Explosion> Explosion
+        {
+            get { return mExplosions; }
+            set { mExplosions = value; }
+        }
+        
+        float fTimer;
+        int iDelay;
+
+        public VisualEffectManager()
+        {
+            mExplosions = new List<Explosion>();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+            foreach (Explosion explos in mExplosions)
+            {
+                explos.Update(gameTime);
+            }
+
+            // Clean up
+            for (int i = mExplosions.Count - 1; i >= 0; i--)
+            {
+                if (mExplosions[i].IsMarkedForDeletion)
+                    mExplosions.RemoveAt(i);
+            }
+        }
+
+        public void AddExplosion(Vector2 origin, Vector2 velocity)
+        {
+            Explosion explos = new Explosion(CUtil.ResourcePool.Explosion1, CUtil.ResourcePool.Explosion1, origin, velocity);
+            mExplosions.Add(explos);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Explosion explos in mExplosions)
+            {
+                explos.Draw(spriteBatch);
+            }
+
+        }
+
+
+    }
+}
